3c1aa8170bd9c065c4460b30f767e035f203ecee
Vinicius Blazius Goulart <viblaziusgoulart@gmail.com>
2023-03-22 21:14:51 -0300

    feat(participant): participant must belong to an event

--- packages/modules/src/participant/ParticipantModel.ts
+++ packages/modules/src/participant/ParticipantModel.ts
@@ -1,9 +1,13 @@
+import { ObjectId } from 'mongodb';
 import type { Document, Types } from 'mongoose';
 import { model, Schema } from 'mongoose';
+
+import type { EventDocument } from '../event/EventModel';
 
 type IParticipant = {
   _id: Types.ObjectId;
   name: string;
+  event: EventDocument['_id'];
   batch: string;
 };
 
@@ -13,6 +17,11 @@ const ParticipantSchema = new Schema<ParticipantDocument>(
   {
     name: {
       type: String,
+      required: true,
+    },
+    event: {
+      type: ObjectId,
+      ref: 'Event',
       required: true,
     },
     batch: {
