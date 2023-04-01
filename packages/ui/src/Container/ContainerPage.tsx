3daba3c71a99876bc3551d36bd8f1e50468c9bfa
Vinicius Blazius Goulart <viblaziusgoulart@gmail.com>
2023-04-01 12:51:24 -0300

    feat(ui): container page description

--- packages/ui/src/Container/ContainerPage.tsx
+++ packages/ui/src/Container/ContainerPage.tsx
@@ -1,14 +1,23 @@
-import { Stack } from '@chakra-ui/react';
+import { Stack, Text } from '@chakra-ui/react';
 
 import { TextDecorated } from '@event-list/ui';
 
-const ContainerPage = ({ title, children }: { title: string; children: React.ReactNode }) => {
+const ContainerPage = ({
+  title,
+  description,
+  children,
+}: {
+  title: string;
+  description?: string;
+  children: React.ReactNode;
+}) => {
   return (
-    <Stack spacing={8}>
-      <Stack align={'center'}>
-        <TextDecorated fontSize={'6xl'} fontWeight={'bold'} textAlign={'center'}>
+    <Stack spacing={8} px={'2rem'}>
+      <Stack>
+        <TextDecorated fontSize={'5xl'} fontWeight={'bold'}>
           {title}
         </TextDecorated>
+        {description && <Text color={'gray.200'}>{description}</Text>}
       </Stack>
       {children}
     </Stack>
