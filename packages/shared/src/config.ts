13cf7b88cdae85503bfc65dc04e2b017651d2388
Vinicius Blazius Goulart <viblaziusgoulart@gmail.com>
2023-04-03 13:58:09 -0300

    fix(cookies): trying to solve cookies

--- packages/shared/src/config.ts
+++ packages/shared/src/config.ts
@@ -14,12 +14,13 @@ export const {
   MONGO_URL,
   JWT_SECRET,
   GRAPHQL_ENDPOINT,
-  EVENT_LIST_DOMAIN,
   DISCORD_GENERAL_WEBHOOK,
   DISCORD_ENTRIES_WEBHOOK,
   DISCORD_COOKIES_WEBHOOK,
 } = process.env;
 
 export const EVENT_LIST_ENV = process.env.EVENT_LIST_ENV || 'development';
+
+export const EVENT_LIST_DOMAIN = process.env.EVENT_LIST_DOMAIN || '.evtlist.com'
 
 export const API_PORT = process.env.API_PORT || 4000;
