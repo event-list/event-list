06d8cd168fbc1e3276a3a7d4be7e5f19184afd5d
Vinicius <viblaziusgoulart@gmail.com>
2023-05-01 12:04:21 -0300

    feat(admin): welcome page

---
+++ apps/admin/src/components/welcome/WelcomeView.tsx
@@ -0,0 +1,59 @@
+import { HStack, Stack } from '@chakra-ui/react';
+import Image from 'next/image';
+import { useTranslation } from 'react-i18next';
+
+import { Hero, Container } from '@event-list/ui';
+
+import HelpImage from '../../../data/images/HelpImage.webp';
+import MyEventsImage from '../../../data/images/MyEventsImage.webp';
+import ProfileImage from '../../../data/images/ProfileImage.webp';
+import ShareEventImage from '../../../data/images/ShareEventImage.webp';
+
+
+const WelcomeView = () => {
+  const { t } = useTranslation(['ptBR', 'en']);
+
+  return (
+    <Hero title={t('Welcome')} description={'Event list is a new way to share your events, enjoy it!'}>
+      <Stack spacing={6}>
+        <Container
+          title={t('Share your event')}
+          description={t('Do not waste time and share your events now')}
+          buttonNav={{
+            href: '/share-your-event',
+            label: t('Share an event')!,
+          }}
+          image={<Image src={ShareEventImage} layout="fill" objectFit="contain" />}
+        />
+        <HStack spacing={6}>
+          <Container
+            title={t('See your events')}
+            description={t('Total confirmed, guest list, price, and more...')}
+            buttonNav={{
+              href: '/events',
+              label: t('My events')!,
+            }}
+            image={<Image src={MyEventsImage} layout="fill" objectFit="contain" />}
+          />
+          <Container
+            title={t('Edit your profile')}
+            description={'Logo, instagram, website, description, and more...'}
+            buttonNav={{
+              href: '/profile',
+              label: t('Profile')!,
+            }}
+            image={<Image src={ProfileImage} layout="fill" objectFit="contain" />}
+          />
+        </HStack>
+        <Container
+          title={t('New here?')}
+          description={t('Start with our help page')}
+          buttonNav={{ href: '/help', label: t('Help')! }}
+          image={<Image src={HelpImage} layout="fill" objectFit="contain" />}
+        />
+      </Stack>
+    </Hero>
+  );
+};
+
+export { WelcomeView };
--- packages/ui/src/Container/Container.tsx
+++ packages/ui/src/Container/Container.tsx
@@ -1,11 +1,10 @@
-import { Box, Flex, Stack, Heading, Text, HStack } from '@chakra-ui/react';
+import { Box, Flex, Stack, Heading, Text, Grid } from '@chakra-ui/react';
 import type { ContainerProps as ContainerPropsChakra } from '@chakra-ui/react';
 
 import type { LinkDecoratedProps } from '../Link/LinkDecorated';
 import LinkDecorated from '../Link/LinkDecorated';
 
 type ContainerProps = {
-  fullSize?: boolean;
   title: string;
   description: string;
   buttonNav: LinkDecoratedProps;
@@ -13,30 +12,11 @@ type ContainerProps = {
 } & ContainerPropsChakra;
 
 const Container = (props: ContainerProps) => {
-  const { fullSize = true, title, description, buttonNav, image } = props;
-
-  if (!fullSize) {
-    return (
-      <Box w="full" rounded="lg" px={8} py={6} bgColor="gray.900">
-        <Stack spacing={6}>
-          <Flex justifyContent="space-between">
-            <Stack>
-              <Heading fontSize={{ base: 'lg', sm: '2xl' }}>{title}</Heading>
-              <Text fontSize="md">{description}</Text>
-            </Stack>
-            <Stack>
-              <LinkDecorated target="_blank" w="10rem" {...buttonNav} />
-            </Stack>
-          </Flex>
-          <Box>{image}</Box>
-        </Stack>
-      </Box>
-    );
-  }
+  const { title, description, buttonNav, image } = props;
 
   return (
-    <Box w="full" rounded="lg" px={8} py={6} bgColor="gray.900">
-      <Flex justifyContent="space-between">
+    <Box w="full" rounded="lg" px={8} py={6} bgColor="gray.900" position="relative">
+      <Grid templateColumns={{ base: '1fr', md: '10fr 1fr' }} gap={4}>
         <Stack spacing={10}>
           <Stack>
             <Heading fontSize={{ base: 'lg', sm: '2xl' }}>{title}</Heading>
@@ -44,8 +24,10 @@ const Container = (props: ContainerProps) => {
           </Stack>
           <LinkDecorated target="_blank" w="10rem" {...buttonNav} />
         </Stack>
-        <Box>{image}</Box>
-      </Flex>
+        <Box width="200px" position="relative">
+          {image}
+        </Box>
+      </Grid>
     </Box>
   );
 };
